a6428d8f09ec15a58fb03db684ee452ffe61b0db
Karan-Developer-69 <homepc8890@gmail.com>
2025-09-06 13:09:03 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import AppRoutes from "./routes/AppRoutes"
 import { AuthProvider } from "./contexts/AuthContext"
+import { CookiesProvider } from 'react-cookie';
 
 function App() {
   const SERVER_URL = import.meta.env.VITE_SERVER_URL;
   console.log(SERVER_URL);
   return (
+    <CookiesProvider defaultSetOptions={{ path: '/' }}>
     <AuthProvider serverUrl={SERVER_URL}>
       <AppRoutes />
     </AuthProvider>
+    </CookiesProvider>
   )
 }
 
